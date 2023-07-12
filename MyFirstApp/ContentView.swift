5574831f2372aebdba88c64542fbd5e25e774332
AnyaILi <anyali0219@gmail.com>
2023-07-12 14:43:59 -0700

    added pretty butterfly

--- MyFirstApp/ContentView.swift
+++ MyFirstApp/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Pretty Butterfly")
+            Text("Pretty Butterfly!")
                 .font(.title)
                 .foregroundColor(Color.purple)
             Image("IMG_0688")
